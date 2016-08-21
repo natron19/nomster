784ac05cbe09cac8a8f7d64ab5b562c4442613af
Nathan Rohm <nathanrohm@gmail.com>
2016-08-21 17:36:08 +0000

    adds index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'places/index'
+  resources :places
+
+  root 'places#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
