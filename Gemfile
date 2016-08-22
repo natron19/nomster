9d4970753121de175a391fd5c070342d4cb95732
Nathan Rohm <nathanrohm@gmail.com>
2016-08-22 20:40:18 +0000

    adds navigationa nd new

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'spring',        group: :development
 gem 'rails_12factor', group: :production
 
 gem 'will_paginate', '~> 3.1.0'
+
+gem 'simple_form'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,25 @@ class PlacesController < ApplicationController
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.create(place_params)
+
+    if @place.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
