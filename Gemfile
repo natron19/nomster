af1e7e72c4336e7b5dcbeffd73322be2cc2f153e
Nathan Rohm <nathanrohm@gmail.com>
2016-08-23 23:01:40 +0000

    adds geocoder and figaro

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'will_paginate', '~> 3.1.0'
 gem 'simple_form'
 
 gem 'devise', '>= 3.2.4'
+
+gem "geocoder", "1.3.4"
+
+gem "figaro", ">= 1.0.0"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,12 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode
+
   validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
+
 end
