c2bb85abbc287f92e988bd9c1cb12d95c49c4d66
Nathan Rohm <nathanrohm@gmail.com>
2016-08-23 16:56:19 +0000

    adds edit and udpate places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,6 +6,10 @@ class PlacesController < ApplicationController
     @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
   def new
     @place = Place.new
   end
@@ -14,11 +18,36 @@ class PlacesController < ApplicationController
     @place = current_user.places.create(place_params)
 
     if @place.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Place successfully added."
     else
       render 'new'
     end
   end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+
+    if @place.update_attributes(place_params)
+      redirect_to root_path, notice: "Place successfully updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+
+    if @place.destroy
+      redirect_to root_path, notice: "Place was successfully deleted."
+    else
+      render 'new'
+    end
+  end
+
 
   private
 
