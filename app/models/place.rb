e891fb70f9c6c2c45698cdb25f387b6fdf55ae84
Nathan Rohm <nathanrohm@gmail.com>
2016-08-24 17:43:23 +0000

    adds comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
 
   geocoded_by :address
   after_validation :geocode
@@ -8,5 +9,4 @@ class Place < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
-
 end
